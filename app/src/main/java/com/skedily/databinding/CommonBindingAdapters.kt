ac8a12619783318854fb86b11eb8cf288ec1caeb
IraSmalko <audir8audir8>
2017-12-02 19:10:17 +0200

    UI + CheckBox

--- app/src/main/java/com/skedily/databinding/CommonBindingAdapters.kt
+++ app/src/main/java/com/skedily/databinding/CommonBindingAdapters.kt
@@ -5,11 +5,8 @@ import android.databinding.BindingAdapter
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Spinner
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions.bitmapTransform
--- app/src/main/java/com/skedily/model/ChecklistItem.kt
+++ app/src/main/java/com/skedily/model/ChecklistItem.kt
@@ -6,6 +6,4 @@ import android.databinding.BaseObservable
  * Created by smalk on 12/1/2017.
  */
 class ChecklistItem(var name: String,
-                    var state: Boolean) : BaseObservable() {
-
-}
+                    var state: Boolean) : BaseObservable()
